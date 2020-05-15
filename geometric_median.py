44a89f517cc13f01d285f7779c16ecf7bf715ae4
Matt Woolley <matt.wool@live.com.au>
2020-05-15 19:23:23 +1000

    First steps

--- geometric_median.py
+++ geometric_median.py
@@ -5,5 +5,32 @@ Date Created:
 Last Modified:      
 """
 
+import numpy as np
+from scipy.spatial.distance import cdist
+
 __author__ = "Matthew Woolley"
 
+def calc_dist2points(curr_point, points, metric='euclidean'):
+    """
+
+    :param curr_point:
+    :param points:
+    :param metric:
+    :return:
+    """
+
+    # Check curr_point is 1D
+    if len(curr_point.shape) > 1:
+        raise ValueError(f"curr_point must be 1D array: curr_point is of shape {curr_point.shape}")
+
+    # Check if points is 2D
+    if len(points.shape) > 2:
+        raise ValueError(f"points must be 2D array: points is of shape {points.shape}")
+
+    # Check for boolean inputs
+
+    return cdist([curr_point], points, metric=metric)
+
+x = np.array([1, 3, 5])
+y = np.array([[2, 5, 1], [1, 1, 9], [4, 2, 1], [5, 9, 1]])
+print(calc_dist2points(x, y))
--- test_geometric_median.py
+++ test_geometric_median.py
@@ -5,5 +5,18 @@ Date Created:
 Last Modified:      
 """
 
+import numpy as np
+import unittest
+
 __author__ = "Matthew Woolley"
+
+
+class TestCalcDist2Points(unittest.TestCase):
+
+    def test_value(self):
+        curr_point = np.array([1, 2, 3])
+        points = np.arr
+        self.assertAlmostEqual()
+
+
 
