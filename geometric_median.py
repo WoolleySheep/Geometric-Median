c56fc6613ea5b7d19aa06053ede8a16957fb6383
Matt Woolley <matt.wool@live.com.au>
2020-05-17 03:46:10 +1000

    weiszfeld and minimize algorithms implemented

--- geometric_median.py
+++ geometric_median.py
@@ -7,30 +7,118 @@ Last Modified:
 
 import numpy as np
 from scipy.spatial.distance import cdist
+from scipy.optimize import minimize
 
 __author__ = "Matthew Woolley"
 
-def calc_dist2points(curr_point, points, metric='euclidean'):
-    """
+def geometric_median(points: np.array, weights: np.array=None, method: str='weiszfeld', convergence_threshold: float=1e-5, iteration_limit: int=1000, dist_measure: str='euclidean', solver_method=None):
 
-    :param curr_point:
-    :param points:
-    :param metric:
-    :return:
-    """
+    # Raise errors for improper inputs
 
-    # Check curr_point is 1D
-    if len(curr_point.shape) > 1:
-        raise ValueError(f"curr_point must be 1D array: curr_point is of shape {curr_point.shape}")
+    # points errors
+    if type(points) is not np.ndarray:
+        raise TypeError(f"Type of points must be a numpy array; current type is {type(points)}")
+    if (np.issubdtype(points.dtype, np.integer) or np.issubdtype(points.dtype, np.floating)) is False:
+        raise TypeError(f"Datatype of points must be int or float; current datatype is {points.dtype}")
+    if len(points.shape) != 2:
+        raise ValueError(f"points must be a 2D array; currently shape is {points.shape}")
 
-    # Check if points is 2D
-    if len(points.shape) > 2:
-        raise ValueError(f"points must be 2D array: points is of shape {points.shape}")
+    npoint, ndim = points.shape
 
-    # Check for boolean inputs
+    if ndim < 1:
+        raise ValueError(f"value of ndim must be >= 1; currently value is {ndim}")
 
-    return cdist([curr_point], points, metric=metric)
+    # weights errors
+    if weights is None:
+        weights = np.ones(npoint)
+    else:
+        if type(weights) is not np.ndarray:
+            raise TypeError(f"Type of weights must be a numpy array; current type is {type(points)}")
+        if (np.issubdtype(weights.dtype, np.integer) or np.issubdtype(weights.dtype, np.floating)) is False:
+            raise TypeError(f"Datatype of points must be real numbers; current datatype is {points.dtype}")
+        if len(weights.shape) != 1:
+            raise ValueError(f"weights must be a 1D array; currently shape is {weights.shape}")
 
-x = np.array([1, 3, 5])
-y = np.array([[2, 5, 1], [1, 1, 9], [4, 2, 1], [5, 9, 1]])
-print(calc_dist2points(x, y))
+    # method errors
+    valid_methods = {'weiszfeld', 'minimize'}
+    if method not in valid_methods:
+        raise ValueError(f"Invalid method given: {method} not in {valid_methods}")
+
+    # convergence_threshold errors
+    if type(convergence_threshold) not in (int, float):
+        raise TypeError(f"Type of convergence_threshold must be int or float; currently of type {type(convergence_threshold)}")
+    if convergence_threshold <= 0:
+        raise ValueError(f"Value of convergence_threshold must be > 0; current value is {convergence_threshold}")
+
+    # iteration_limit errors
+    if type(iteration_limit) is not int:
+        raise TypeError(f"Type of iteration_limit must be int; current type is {type(iteration_limit)}")
+    if iteration_limit <= 0:
+        raise ValueError(f"Value of iteration_limit must be > 0; current value is {iteration_limit}")
+
+    # distance_measure errors
+    # Caught by scipy.spatial.distance.cdist
+
+    # solver_method errors
+    # Caught by scipy.optimize.minimize
+
+    if method == 'weiszfeld':
+        return weiszfeld_algorithm(points, weights, convergence_threshold, iteration_limit, dist_measure)
+    elif method == 'minimize':
+        return minimize_algorithm(points, weights, convergence_threshold, iteration_limit, dist_measure, solver_method)
+
+
+def weiszfeld_algorithm(points: np.ndarray, weights: np.array, convergence_threshold: float, iteration_limit: int, dist_measure: str) -> np.array:
+
+
+    # Find the weighted centroid and set as the initial center
+    curr_center = (weights[:, None] * points).sum(axis=0) / weights.sum()
+
+    move_dist = float('inf')
+    for _ in range(iteration_limit):
+        if move_dist < convergence_threshold:
+            return curr_center
+        # If curr_center is the same as one of the points, move the current center slightly away from that point
+        # The weiszfeld algorithm will fail to converge if it gets stuck on one of the points
+        if np.isclose(points, curr_center).all(axis=1).any():
+            _, ndim = points.shape
+            dim = np.random.choice(range(ndim))
+            dir = np.random.choice([1, -1])
+            curr_center[dim] += dir * convergence_threshold / 2
+        prev_center = curr_center
+        # Calculate the weighted distances from the current center to all points
+        weighted_distances = cdist(np.array([prev_center]), points, metric=dist_measure) / weights
+        # Get new center prediction
+        curr_center = (points / weighted_distances.T).sum(axis=0) / (1.0 / weighted_distances).sum()
+        # Calculate the distance between the current center and the previous center
+        move_dist = cdist(np.array([curr_center]), np.array([prev_center]), metric=dist_measure)[0]
+
+    raise ValueError(f"Weiszfelds algorithm not able to converge within {iteration_limit} iterations")
+
+
+def minimize_algorithm(points: np.ndarray, weights: np.array, convergence_threshold: float, iteration_limit: int, dist_measure: str, solver_method: str) -> np.array:
+
+
+    def calc_weighted_distance(curr_center):
+
+        return (weights * cdist(np.array([curr_center]), points, metric=dist_measure)).sum()
+
+    # Find the weighted centroid and set as the initial center
+    curr_center = (weights[:, None] * points).sum(axis=0) / weights.sum()
+
+    optimize_result = minimize(calc_weighted_distance, curr_center, method=solver_method, tol=convergence_threshold, options={'maxiter': iteration_limit})
+
+    return optimize_result.x
+
+
+def predict_optiomal_method(npoint, ndim, convergence_threshold):
+
+    # TODO: Need to find time complexity estimates of methods
+    methods = ["Weizfeld", "Vardi-Zhang"]
+
+    time = []
+    # Weizfeld
+    # Vardi-Zhang
+
+    return
+
--- test_geometric_median.py
+++ test_geometric_median.py
@@ -1,16 +1,125 @@
 #!/usr/bin/env python
 """
-Description:        
-Date Created:       
-Last Modified:      
+Description: Tests for geometric_median.py
+Date Created: 16-MAY-2020
+Last Modified: 17-MAY-2020
 """
 __author__ = "Matthew Woolley"
 
 
 import numpy as np
 import unittest
+from geometric_median import geometric_median
 
 
-class TestCalcDist2Points(unittest.TestCase):
+class TestGeometricMedian(unittest.TestCase):
 
-    pass
+    def test_point_type(self):
+        # Non numpy array raises Type Error
+        self.assertRaises(TypeError, geometric_median, "Drax")
+        self.assertRaises(TypeError, geometric_median, 1)
+        self.assertRaises(TypeError, geometric_median, 1.0)
+        self.assertRaises(TypeError, geometric_median, True)
+        self.assertRaises(TypeError, geometric_median, [])
+        self.assertRaises(TypeError, geometric_median, {})
+        self.assertRaises(TypeError, geometric_median, dict())
+        self.assertRaises(TypeError, geometric_median, (1,))
+        # Non int or float dtype raises Type Error
+        self.assertRaises(TypeError, geometric_median, np.array([], dtype=complex))
+        self.assertRaises(TypeError, geometric_median, np.array([], dtype=bool))
+        self.assertRaises(TypeError, geometric_median, np.array([], dtype=str))
+        self.assertRaises(TypeError, geometric_median, np.array([], dtype=dict))
+        self.assertRaises(TypeError, geometric_median, np.array([], dtype=list))
+        self.assertRaises(TypeError, geometric_median, np.array([], dtype=set))
+        self.assertRaises(TypeError, geometric_median, np.array([], dtype=tuple))
+        self.assertRaises(TypeError, geometric_median, np.array([], dtype=object))
+
+    def test_point_value(self):
+        # Non 2D array raises Value Error
+        self.assertRaises(ValueError, geometric_median, np.array([1, 2]))
+        self.assertRaises(ValueError, geometric_median, np.array([[[]]]))
+        # Not having >= one point raises Value Error
+        self.assertRaises(ValueError, geometric_median, np.array([]))
+        # Not having >= one dimension raises Value Error
+        self.assertRaises(ValueError, geometric_median, np.array([[], []]))
+
+    def test_weight_type(self):
+        points = np.array([[1, 2], [2, 1], [2, 8]])
+        self.assertRaises(TypeError, geometric_median, points, weights="Drax")
+        self.assertRaises(TypeError, geometric_median, points, weights=1)
+        self.assertRaises(TypeError, geometric_median, points, weights=1.0)
+        self.assertRaises(TypeError, geometric_median, points, weights=True)
+        self.assertRaises(TypeError, geometric_median, points, weights=[])
+        self.assertRaises(TypeError, geometric_median, points, weights={})
+        self.assertRaises(TypeError, geometric_median, points, weights=dict())
+        self.assertRaises(TypeError, geometric_median, points, weights=(1,))
+        # Non int or float dtype raises Type Error
+        self.assertRaises(TypeError, geometric_median, points, weights=np.array([], dtype=complex))
+        self.assertRaises(TypeError, geometric_median, points, weights=np.array([], dtype=bool))
+        self.assertRaises(TypeError, geometric_median, points, weights=np.array([], dtype=str))
+        self.assertRaises(TypeError, geometric_median, points, weights=np.array([], dtype=dict))
+        self.assertRaises(TypeError, geometric_median, points, weights=np.array([], dtype=list))
+        self.assertRaises(TypeError, geometric_median, points, weights=np.array([], dtype=set))
+        self.assertRaises(TypeError, geometric_median, points, weights=np.array([], dtype=tuple))
+        self.assertRaises(TypeError, geometric_median, points, weights=np.array([], dtype=object))
+
+    def test_weights_value(self):
+        points = np.array([[1, 2], [2, 1], [2, 8]])
+        # Non 1D array raises Value Error
+        self.assertRaises(ValueError, geometric_median, points, weights=np.array([[1], [2]]))
+        # Not having >= one point raises Value Error
+        self.assertRaises(ValueError, geometric_median, points, weights=np.array([]))
+
+    def test_method_value(self):
+        points = np.array([[1, 2], [2, 1], [2, 8]])
+        # Object not in valid_methods list
+        self.assertRaises(ValueError, geometric_median, points, method='Drax')
+
+    def test_convergence_threshold_type(self):
+        points = np.array([[1, 2], [2, 1], [2, 8]])
+        # Non int or float type raises Type Error
+        self.assertRaises(TypeError, geometric_median, points, convergence_threshold="Drax")
+        self.assertRaises(TypeError, geometric_median, points, convergence_threshold=True)
+        self.assertRaises(TypeError, geometric_median, points, convergence_threshold=[])
+        self.assertRaises(TypeError, geometric_median, points, convergence_threshold={})
+        self.assertRaises(TypeError, geometric_median, points, convergence_threshold=dict())
+        self.assertRaises(TypeError, geometric_median, points, convergence_threshold=(1,))
+
+    def test_convergence_threshold_value(self):
+        points = np.array([[1, 2], [2, 1], [2, 8]])
+        # Value <= 0 raises Value Error
+        self.assertRaises(ValueError, geometric_median, points, convergence_threshold=0)
+        self.assertRaises(ValueError, geometric_median, points, convergence_threshold=-1)
+        self.assertRaises(ValueError, geometric_median, points, convergence_threshold=-1.0)
+        self.assertRaises(ValueError, geometric_median, points, convergence_threshold=-0.00001)
+
+    def test_iteration_limit_type(self):
+        points = np.array([[1, 2], [2, 1], [2, 8]])
+        # Non int or float type raises Type Error
+        self.assertRaises(TypeError, geometric_median, points, iteration_limit="Drax")
+        self.assertRaises(TypeError, geometric_median, points, iteration_limit=1.0)
+        self.assertRaises(TypeError, geometric_median, points, iteration_limit=True)
+        self.assertRaises(TypeError, geometric_median, points, iteration_limit=[])
+        self.assertRaises(TypeError, geometric_median, points, iteration_limit={})
+        self.assertRaises(TypeError, geometric_median, points, iteration_limit=dict())
+        self.assertRaises(TypeError, geometric_median, points, iteration_limit=(1,))
+
+    def test_iteration_limit_value(self):
+        points = np.array([[1, 2], [2, 1], [2, 8]])
+        # Value <= 0 raises Value Error
+        self.assertRaises(ValueError, geometric_median, points, iteration_limit=0)
+        self.assertRaises(ValueError, geometric_median, points, iteration_limit=-1)
+
+    def test_dist_measure_type(self):
+        points = np.array([[1, 2], [2, 1], [2, 8]])
+        # String not in valid list (scipy.spatial.distance.cdist)
+        self.assertRaises(TypeError, geometric_median, points, solver='weiszfeld', dist_measure='Drax')
+
+    def test_solver_method_value(self):
+        points = np.array([[1, 2], [2, 1], [2, 8]])
+        # String not in valid list (scipy.optimize.minimize)
+        self.assertRaises(TypeError, geometric_median, points, solver='minimize', solver_method="Drax")
+
+
+
+
